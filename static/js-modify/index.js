44f0a71d255282a2e24015391a417bedaf6f96cb
wang125309 <wang_125309@foxmail.com>
2015-11-17 17:12:02 +0800

    init

---
+++ static/js-modify/index.js
@@ -0,0 +1,11 @@
+require("../../bower_components/zepto/zepto.js");
+require("../../bower_components/zeptojs/src/touch.js");
+require("../../bower_components/velocity/velocity.min.js");
+require("../../bower_components/angular/angular.js");
+require("../js/share.min.js");
+
+indexCtrl = angular.module('app').controller('indexCtrl',['$scope',function($scope){
+
+}]);
+
+indexCtrl.$inject = ['$scope','indexCtrl']; 
